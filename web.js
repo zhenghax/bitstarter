9f0089b0ff9c614dbdd68993b3f87b75b2cd2be2
Zhenghang Xu <zhenghax@gmail.com>
2013-07-11 09:08:59 +0000

    hw3-1-v1

--- web.js
+++ web.js
@@ -2,11 +2,22 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var file = "index.html";
+ buffer = new Buffer(fs.readFileSync(file));
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  // var fs = require('fs');
+  // var file = "index.html";
+  // buffer = new Buffer(fs.readFileSync(file)); 
+
+   response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
